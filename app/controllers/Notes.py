c49c58f5c221c163287d51372acad899b0dc790d
Michael Arbogast <marboga@gmail.com>
2016-03-24 10:05:29 -0700

    AJAX functionality implemented. Works as desired.

--- app/controllers/Notes.py
+++ app/controllers/Notes.py
@@ -4,10 +4,7 @@ from system.core.controller import *
 class Notes(Controller):
     def __init__(self, action):
         super(Notes, self).__init__(action)
-
         self.load_model('Note')
-
-
 
     def index(self):
         notes = self.models['Note'].get_all_notes()
@@ -30,10 +27,11 @@ class Notes(Controller):
         "description": request.form['description'],
         "id": request.form['hidden']
         }
-        print "desc"
         self.models['Note'].add_new_description(desc)
-        return redirect('/')
+        notes = self.models['Note'].get_all_notes()
+        return self.load_view('partialindex.html', notes=notes)
 
     def delete(self, id):
         self.models['Note'].delete_note(id)
-        return redirect('/')
+        notes = self.models['Note'].get_all_notes()
+        return self.load_view('partialindex.html', notes=notes)
